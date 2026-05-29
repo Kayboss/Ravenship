35a661ec29ce20f806307290089b1b501acb3321
Kayboss <zac@admin.com>
2026-05-29 00:19:49 +0000

    fix: use builds config for Vercel to include serverless functions

--- mentorship-app/api/index.js
+++ mentorship-app/api/index.js
@@ -1,3 +1,10 @@
 import app from "../src/server.js";
 
+app.use((req, res, next) => {
+  if (req.url !== "/" && !req.url.startsWith("/api") && !req.path.startsWith("/api")) {
+    req.url = "/api" + req.url;
+  }
+  next();
+});
+
 export default app;
